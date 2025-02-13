180034e06753f63da170ea89daccd1b83a8e0a69
Andrewf2929 <afernandes0101@gmail.com>
2025-02-13 15:38:38 -0500

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -2,9 +2,13 @@
 
 import java.util.Random;
 import java.util.Scanner;
-
+  
 public class Assignment1 {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+    
+
     System.out.println("RRRRRRRRR      VVV         VVV");
         System.out.println("RRR     RRR     VVV       VVV");
         System.out.println("RRR     RRR      VVV     VVV");
@@ -13,7 +17,16 @@ public class Assignment1 {
         System.out.println("RRR     RRR         VVVVV");
         System.out.println("RRR     RRR          VVV");
         
-    
+      System.out.println("Please enter a number in Farenheit: ");
+        int farenheit = scanner.nextInt();
 
+      System.out.println("Please enter a 5-character string: ");
+        String inputString = scanner.next();
+
+      System.out.println("Generating random number. Continuing... ");
+        double randomNumber = random.nextDouble();
+
+      System.out.println("Your new string is" + randomNumber + inputString);
+    
   }
 }
